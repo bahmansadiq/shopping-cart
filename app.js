10924b49cf3f9bc79a076e8d74c59232728ba796
Sadiq <BSadiq@destinationhotels.com>
2017-11-14 16:52:34 -0800

    add signin route and protect route

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session=require('express-session');
 var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 var passport= require('passport');
 var flash= require('flash');
+var validator= require('express-validator');
 
 var app = express();
 
@@ -25,12 +27,19 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret:'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function(req, res, next){
+  res.locals.login= req.isAuthenticated();
+  next();
+});
+app.use('/user', userRoutes);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var csrf=require('csurf');
-var passport=require('passport');
 var Product = require('../models/product');
-var csrfProtection=csrf();
 
-router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
@@ -18,21 +14,5 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/user/signup', function(req, res, next){
-  var messages= req.flash('error');
-  res.render('user/signup.hbs', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0});
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-    successRedirect:'/user/profile',
-    failureRedirect:'/user/signup',
-    failureFlash: true
-
-}));
-
-router.get('/user/profile', function(req, res, next){
-  res.render('user/profile');
-
-});
 
 module.exports = router;
