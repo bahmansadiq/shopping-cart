2ffbb6a6f5028f612edd3bb5b9fb484f7bf0f85f
Sadiq <BSadiq@destinationhotels.com>
2017-11-29 15:31:51 -0800

    troubleshout the total price in the cart

--- models/cart.js
+++ models/cart.js
@@ -2,14 +2,14 @@ module.exports= function Cart(oldCart){
 	this.items=oldCart.items || {};
 	this.totalQty=oldCart.totalQty || 0;
 	this.totalPrice=oldCart.totalPrice || 0;
-	this.add=function(item, id){
+	this.add = function(item, id){
 		var storedItem= this.items[id];
 		if(!storedItem){
 			storedItem= this.items[id]={item: item, qty:0, price:0}
 		}
-		storedItem.qty++;
+		storedItem.qty ++;
 		storedItem.price= storedItem.item.price*storedItem.qty;
-		this.totalQty++;
+		this.totalQty ++;
 		this.totalPrice += storedItem.item.price;
 	}
 
--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/add-to-cart/:id', function(req, res, next){
   var productId=req.params.id;
-  var cart= new Cart(req.session.cart ? req.session.cart : {});
+  var cart= new Cart(req.session.cart ? req.session.cart:{});
   Product.findById(productId, function(err, product){
     if(err) {
       return res.redirect('/');
